182eb8c7ae7a1b7a14c7d5e8490a1c35cdddfb28
paul-caveim <paul@caveim.com>
2025-03-24 20:25:21 +0800

    update

--- app/Http/Controllers/TradePairAccountsController.php
+++ app/Http/Controllers/TradePairAccountsController.php
@@ -328,7 +328,7 @@ class TradePairAccountsController extends Controller
                 'status' => $item->status,
                 'errors' => maybe_unserialize($item->errors),
                 'pair_status' => $item->pair_status,
-                'closed_items' => $item->closed_items
+                'closed_items' => maybe_unserialize($item->closed_items)
             ];
 
             if (in_array($item->status, $pairStatuses)) {
