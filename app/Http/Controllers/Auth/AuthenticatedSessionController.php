3b861c6793e7e8d6aa4f054f14cff10e6dfaaf09
pegj44 <31127167+pegj44@users.noreply.github.com>
2025-06-24 09:05:52 +0800

    Staging (#7)
    * auto update status on manual equity input (#1)
    
    * Staging paul (#3)
    
    * auto update status on manual equity input
    
    * add pusher details to disruptor helper api response
    
    * Staging paul (#4)
    
    * auto update status on manual equity input
    
    * add pusher details to disruptor helper api response
    
    * update helper api response
    
    * Staging paul (#5)
    
    * auto update status on manual equity input
    
    * add pusher details to disruptor helper api response
    
    * update helper api response
    
    * add margin cap functionality

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -134,6 +134,10 @@ class AuthenticatedSessionController extends Controller
             }
 
             $data = [
+                'PUSHER_APP_ID' => env('PUSHER_APP_ID'),
+                'PUSHER_APP_KEY' => env('PUSHER_APP_KEY'),
+                'PUSHER_APP_SECRET' => env('PUSHER_APP_SECRET'),
+                'PUSHER_APP_CLUSTER' => env('PUSHER_APP_CLUSTER'),
                 'token' => $userToken,
                 'userId' => $token->tokenable->id,
                 'name' => $token->tokenable->name,
@@ -156,6 +160,10 @@ class AuthenticatedSessionController extends Controller
             $newToken = $user->createToken($tokenName, ['unit'])->plainTextToken;
 
             $data = [
+                'PUSHER_APP_ID' => env('PUSHER_APP_ID'),
+                'PUSHER_APP_KEY' => env('PUSHER_APP_KEY'),
+                'PUSHER_APP_SECRET' => env('PUSHER_APP_SECRET'),
+                'PUSHER_APP_CLUSTER' => env('PUSHER_APP_CLUSTER'),
                 'token' => $newToken,
                 'userId' => $user->id,
                 'name' => $user->name,
